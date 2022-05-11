e08c66afb8fde73190079836d0d8043df3fad4a2
Manuel <martinezm731@outlook.com>
2022-05-10 21:38:35 -0400

    removed clone git repo to place contents in my repo

---
+++ shopping-app/Redux-Shopping-Cart-App/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import "./App.css";
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
+
+function App() {
+  const cart = useSelector((state) => state.cart);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  useEffect(() => {
+    fetch(
+      "https://redux-shopping-cart-f2ae7-default-rtdb.firebaseio.com/cartItems.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
+
+  // console.log(isLoggedIn);
+  // const cartItems = useSelector((state) => state.cart.itemsList)
+  // console.log(cartItems);
+
+  return (
+    <div className="App">
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
+    </div>
+  );
+}
+
+export default App;
