99e87749afb82b0a0d9f8c0df52d6a6f9fc94310
Manuel <martinezm731@outlook.com>
2022-05-17 08:54:56 -0400

    notification functionality complete

--- shopping-app/src/App.js
+++ shopping-app/src/App.js
@@ -1,14 +1,30 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import Notification from "./components/Notification";
+import { uiActions } from "./store/ui-slice";
+let isFirstRender = true
 
 function App() {
+  const dispatch = useDispatch()
+  const notification = useSelector(state => state.ui.notification)
   const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   useEffect(() => {
+    if (isFirstRender) {
+      isFirstRender = false
+      return 
+    }
+
     const sendRequest = async () => {
+      //Send state as sending request
+      dispatch(uiActions.showNotification({
+        open: true,
+        message: 'Sending Request',
+        type: 'warning'
+      }))
       const res = await fetch(
         "https://redux-shopping-cart-f2ae7-default-rtdb.firebaseio.com/cartItems.json",
         {
@@ -17,8 +33,20 @@ function App() {
         }
       );
       const data = await res.json();
+      //Send state as request is successful
+      dispatch(uiActions.showNotification({
+        open: true,
+        message: 'Sent Request to Database',
+        type: 'success'
+      }))
     };
-    sendRequest()
+    sendRequest().catch(err => {
+      dispatch(uiActions.showNotification({
+        open: true,
+        message: 'Sending Request Failed',
+        type: 'error'
+      }))
+    })
   }, [cart]);
 
   // console.log(isLoggedIn);
@@ -27,6 +55,7 @@ function App() {
 
   return (
     <div className="App">
+      {notification && <Notification type={notification.type} message={notification.message}/>}
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
